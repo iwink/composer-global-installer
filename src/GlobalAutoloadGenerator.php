95485a4288d68e9fd3e97e9f95f146559be37e60
Sjoerd Adema <s.j.adema@iwink.nl>
2021-08-26 14:51:48 +0200

    Feature/psr 12 (#10)
    * Adhere to PSR-12
    
    * Update CHANGELOG
    
    * Declare strict types
    
    * Cleaner code
    
    * Add phpcs workflow
    
    * Create phpcs.xml
    
    * Update main.yml
    
    * Update phpcs.xml
    
    * Update src/Plugin.php
    
    Co-authored-by: Daniel Koster <me@danielkoster.nl>

--- src/GlobalAutoloadGenerator.php
+++ src/GlobalAutoloadGenerator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Iwink\ComposerGlobalInstaller;
 
 use Composer\Autoload\AutoloadGenerator;
@@ -14,74 +16,84 @@ use Composer\Util\Filesystem;
  * Autoload generator for globally installed packages.
  * @since 1.0.0
  */
-class GlobalAutoloadGenerator extends AutoloadGenerator {
-	/**
-	 * Array of globally installed package paths.
-	 * @since 1.0.0
-	 * @var string[]
-	 */
-	private array $globalPaths = [];
+class GlobalAutoloadGenerator extends AutoloadGenerator
+{
+    /**
+     * Array of globally installed package paths.
+     * @since 1.0.0
+     * @var string[]
+     */
+    private array $globalPaths = [];
 
-	/**
-	 * @inheritDoc
-	 *
-	 * Builds an array of globally installed package-paths which is used to determine the autoloader paths.
-	 *
-	 * @since 1.0.0
-	 */
-	public function dump(
-		Config $config,
-		InstalledRepositoryInterface $localRepo,
-		RootPackageInterface $rootPackage,
-		InstallationManager $installationManager,
-		$targetDir,
-		$scanPsrPackages = false,
-		$suffix = ''
-	): int {
-		$this->globalPaths = array_map(
-			static fn(PackageInterface $p): string => $p->getDistUrl(),
-			array_filter(
-				$localRepo->getCanonicalPackages(),
-				static function (PackageInterface $p) use ($installationManager): bool {
-					// We can skip if the installer doesn't support the package
-					$installer = $installationManager->getInstaller($p->getType());
-					if (!$installer instanceof GlobalInstaller || !$installer->supports($p->getType())) {
-						return false;
-					}
+    /**
+     * @inheritDoc
+     *
+     * Builds an array of globally installed package-paths which is used to determine the autoloader paths.
+     *
+     * @since 1.0.0
+     */
+    public function dump(
+        Config $config,
+        InstalledRepositoryInterface $localRepo,
+        RootPackageInterface $rootPackage,
+        InstallationManager $installationManager,
+        $targetDir,
+        $scanPsrPackages = false,
+        $suffix = ''
+    ): int {
+        $this->globalPaths = array_map(
+            static fn(PackageInterface $p): string => $p->getDistUrl(),
+            array_filter(
+                $localRepo->getCanonicalPackages(),
+                static function (PackageInterface $p) use ($installationManager): bool {
+                    // We can skip if the installer doesn't support the package
+                    $installer = $installationManager->getInstaller($p->getType());
+                    if (!$installer instanceof GlobalInstaller || !$installer->supports($p->getType())) {
+                        return false;
+                    }
 
-					// We can skip if the package isn't an absolute symlink
-					if ('path' !== $p->getDistType() || ($p->getTransportOptions()['relative'] ?? true)) {
-						return false;
-					}
+                    // We can skip if the package isn't an absolute symlink
+                    if ('path' !== $p->getDistType() || ($p->getTransportOptions()['relative'] ?? true)) {
+                        return false;
+                    }
 
-					// We can skip if the dist URL isn't our global installer path
-					if (0 !== strpos($p->getDistUrl(), $installer->getBasePath())) {
-						return false;
-					}
+                    // We can skip if the dist URL isn't our global installer path
+                    if (0 !== strpos($p->getDistUrl(), $installer->getBasePath())) {
+                        return false;
+                    }
 
-					return true;
-				}
-			)
-		);
+                    return true;
+                }
+            )
+        );
 
-		return parent::dump($config, $localRepo, $rootPackage, $installationManager, $targetDir, $scanPsrPackages, $suffix);
-	}
+        return parent::dump(
+            $config,
+            $localRepo,
+            $rootPackage,
+            $installationManager,
+            $targetDir,
+            $scanPsrPackages,
+            $suffix
+        );
+    }
 
-	/**
-	 * @inheritDoc
-	 *
-	 * If the requested path is a valid global install path, return it without modification.
-	 *
-	 * @since 1.0.0
-	 */
-	protected function getPathCode(Filesystem $filesystem, $basePath, $vendorPath, $path): string {
-		$resolvedPath = realpath($path);
-		foreach ($this->globalPaths as $globalPath) {
-			if (0 === strpos($resolvedPath, $globalPath)) {
-				return var_export($filesystem->normalizePath($resolvedPath), true);
-			}
-		}
+    /**
+     * @inheritDoc
+     *
+     * If the requested path is a valid global install path, return it without modification.
+     *
+     * @since 1.0.0
+     */
+    protected function getPathCode(Filesystem $filesystem, $basePath, $vendorPath, $path): string
+    {
+        $resolvedPath = realpath($path);
+        foreach ($this->globalPaths as $globalPath) {
+            if (0 === strpos($resolvedPath, $globalPath)) {
+                return var_export($filesystem->normalizePath($resolvedPath), true);
+            }
+        }
 
-		return parent::getPathCode($filesystem, $basePath, $vendorPath, $path);
-	}
+        return parent::getPathCode($filesystem, $basePath, $vendorPath, $path);
+    }
 }
--- src/GlobalInstaller.php
+++ src/GlobalInstaller.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Iwink\ComposerGlobalInstaller;
 
 use Composer\Composer;
@@ -17,190 +19,197 @@ use React\Promise\PromiseInterface;
  * Global installer.
  * @since 1.0.0
  */
-class GlobalInstaller extends LibraryInstaller {
-	/**
-	 * Path to project directory.
-	 * @since 1.0.0
-	 * @var string
-	 */
-	private string $projectPath;
+class GlobalInstaller extends LibraryInstaller
+{
+    /**
+     * Path to project directory.
+     * @since 1.0.0
+     * @var string
+     */
+    private string $projectPath;
 
-	/**
-	 * Path to global directory.
-	 * @since 1.0.0
-	 * @var string
-	 */
-	private string $globalPath;
+    /**
+     * Path to global directory.
+     * @since 1.0.0
+     * @var string
+     */
+    private string $globalPath;
 
-	/**
-	 * Array of excluded packages (including vendor/ prefix).
-	 * @since 1.0.0
-	 * @var string[]
-	 */
-	private array $excludedPackages;
+    /**
+     * Array of excluded packages (including vendor/ prefix).
+     * @since 1.0.0
+     * @var string[]
+     */
+    private array $excludedPackages;
 
-	/**
-	 * Creates a new installer.
-	 * @since 1.0.0
-	 * @param IOInterface $io IO.
-	 * @param Composer $composer Composer.
-	 * @param string $projectPath Path to project directory.
-	 * @param string $globalPath Path to global directory.
-	 * @param string[] $excludedPackages Array of excluded packages.
-	 */
-	public function __construct(
-		IOInterface $io,
-		Composer $composer,
-		string $projectPath,
-		string $globalPath,
-		array $excludedPackages
-	) {
-		parent::__construct($io, $composer, null, new Filesystem(new ProcessExecutor($io)));
+    /**
+     * Creates a new installer.
+     * @since 1.0.0
+     * @param IOInterface $io IO.
+     * @param Composer $composer Composer.
+     * @param string $projectPath Path to project directory.
+     * @param string $globalPath Path to global directory.
+     * @param string[] $excludedPackages Array of excluded packages.
+     */
+    public function __construct(
+        IOInterface $io,
+        Composer $composer,
+        string $projectPath,
+        string $globalPath,
+        array $excludedPackages
+    ) {
+        parent::__construct($io, $composer, null, new Filesystem(new ProcessExecutor($io)));
 
-		$this->projectPath = $projectPath;
-		$this->globalPath = rtrim($globalPath, '/');
-		$this->excludedPackages = $excludedPackages;
-	}
+        $this->projectPath = $projectPath;
+        $this->globalPath = rtrim($globalPath, '/');
+        $this->excludedPackages = $excludedPackages;
+    }
 
-	/**
-	 * @inheritDoc
-	 * @since 1.0.0
-	 */
-	public function supports($packageType): bool {
-		return !in_array($packageType, ['composer-installer', 'composer-plugin', 'metapackage']);
-	}
+    /**
+     * @inheritDoc
+     * @since 1.0.0
+     */
+    public function supports($packageType): bool
+    {
+        return !in_array($packageType, ['composer-installer', 'composer-plugin', 'metapackage']);
+    }
 
-	/**
-	 * @inheritDoc
-	 * @since 1.0.0
-	 */
-	public function getInstallPath(PackageInterface $package): string {
-		// Path repositories are always installed locally
-		if ('path' === $package->getDistType()) {
-			return parent::getInstallPath($package);
-		}
+    /**
+     * @inheritDoc
+     * @since 1.0.0
+     */
+    public function getInstallPath(PackageInterface $package): string
+    {
+        // Path repositories are always installed locally
+        if ('path' === $package->getDistType()) {
+            return parent::getInstallPath($package);
+        }
 
-		return $this->supportsGlobalPath($package) ? $this->getGlobalPath($package) : parent::getInstallPath($package);
-	}
+        return $this->supportsGlobalPath($package) ? $this->getGlobalPath($package) : parent::getInstallPath($package);
+    }
 
-	/**
-	 * @inheritDoc
-	 *
-	 * Check both global & local paths for supported packages.
-	 *
-	 * @since 1.0.0
-	 */
-	public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package): bool {
-		if (!$this->supportsGlobalPath($package)) {
-			return parent::isInstalled($repo, $package);
-		}
+    /**
+     * @inheritDoc
+     *
+     * Check both global & local paths for supported packages.
+     *
+     * @since 1.0.0
+     */
+    public function isInstalled(InstalledRepositoryInterface $repo, PackageInterface $package): bool
+    {
+        if (!$this->supportsGlobalPath($package)) {
+            return parent::isInstalled($repo, $package);
+        }
 
-		return parent::isInstalled($repo, $package) && Filesystem::isReadable(parent::getInstallPath($package));
-	}
+        return parent::isInstalled($repo, $package) && Filesystem::isReadable(parent::getInstallPath($package));
+    }
 
-	/**
-	 * @inheritDoc
-	 *
-	 * For supported packages, make sure they're installed globally. The locally installed packages are symlinks.
-	 *
-	 * @since 1.0.0
-	 */
-	public function download(PackageInterface $package, PackageInterface $prevPackage = null): ?PromiseInterface {
-		if (!$this->supportsGlobalPath($package)) {
-			return parent::download($package, $prevPackage);
-		}
+    /**
+     * @inheritDoc
+     *
+     * For supported packages, make sure they're installed globally. The locally installed packages are symlinks.
+     *
+     * @since 1.0.0
+     */
+    public function download(PackageInterface $package, PackageInterface $prevPackage = null): ?PromiseInterface
+    {
+        if (!$this->supportsGlobalPath($package)) {
+            return parent::download($package, $prevPackage);
+        }
 
-		// Make sure the package is installed globally
-		$globalPath = $this->getGlobalPath($package);
-		if (!Filesystem::isReadable($globalPath)) {
-			// To make things even faster, download to system temp directory:
-			// 1. We can't change the download path so temporary move the vendor directory for this project
-			$config = $this->composer->getConfig();
-			$vendor_dir = $config->get('vendor-dir');
-			$config->merge([
-				'config' => [
-					'vendor-dir' => sys_get_temp_dir() . '/composer/' . md5($this->projectPath) . '/vendor',
-				],
-			]);
+        // Make sure the package is installed globally
+        $globalPath = $this->getGlobalPath($package);
+        if (!Filesystem::isReadable($globalPath)) {
+            // To make things even faster, download to system temp directory:
+            // 1. We can't change the download path so temporary move the vendor directory for this project
+            $config = $this->composer->getConfig();
+            $oldVendorDir = $config->get('vendor-dir');
 
-			// 2. Download & install package to global path
-			SyncHelper::downloadAndInstallPackageSync(
-				$this->composer->getLoop(),
-				$this->composer->getDownloadManager()->getDownloader($package->getDistType()),
-				$globalPath,
-				$package
-			);
+            $newVendorDir = sys_get_temp_dir() . '/composer/' . md5($this->projectPath) . '/vendor';
+            $config->merge(['config' => ['vendor-dir' => $newVendorDir]]);
 
-			// 3. Swap vendor dir back to previous
-			$config->merge(['config' => ['vendor-dir' => $vendor_dir]]);
-		}
+            // 2. Download & install package to global path
+            SyncHelper::downloadAndInstallPackageSync(
+                $this->composer->getLoop(),
+                $this->composer->getDownloadManager()->getDownloader($package->getDistType()),
+                $globalPath,
+                $package
+            );
 
-		// Change package to path repository and install locally
-		$package->setDistType('path');
-		$package->setDistUrl($globalPath);
-		$package->setTransportOptions(['relative' => false]);
+            // 3. Swap vendor dir back to previous
+            $config->merge(['config' => ['vendor-dir' => $oldVendorDir]]);
+        }
 
-		return parent::download($package, $prevPackage);
-	}
+        // Change package to path repository and install locally
+        $package->setDistType('path');
+        $package->setDistUrl($globalPath);
+        $package->setTransportOptions(['relative' => false]);
 
-	/**
-	 * @inheritDoc
-	 *
-	 * Remove symlinks to globally installed packages.
-	 *
-	 * @since 1.0.0
-	 */
-	protected function removeCode(PackageInterface $package): ?PromiseInterface {
-		$promise = parent::removeCode($package);
-		if (!$this->supportsGlobalPath($package) || 'path' !== $package->getDistType()) {
-			return $promise;
-		}
+        return parent::download($package, $prevPackage);
+    }
 
-		$path = $this->getInstallPath($package);
-		if ($this->filesystem->isSymlinkedDirectory($path)) {
-			if (!$promise instanceof PromiseInterface) {
-				$promise = \React\Promise\resolve();
-			}
+    /**
+     * @inheritDoc
+     *
+     * Remove symlinks to globally installed packages.
+     *
+     * @since 1.0.0
+     */
+    protected function removeCode(PackageInterface $package): ?PromiseInterface
+    {
+        $promise = parent::removeCode($package);
+        if (!$this->supportsGlobalPath($package) || 'path' !== $package->getDistType()) {
+            return $promise;
+        }
 
-			$promise->then(function () use ($package): void {
-				$this->filesystem->remove($this->getInstallPath($package));
-				$this->io->write(sprintf('  - %s, removing symlink', UninstallOperation::format($package)));
-			});
-		}
+        $path = $this->getInstallPath($package);
+        if ($this->filesystem->isSymlinkedDirectory($path)) {
+            if (!$promise instanceof PromiseInterface) {
+                $promise = \React\Promise\resolve();
+            }
 
-		return $promise;
-	}
+            $promise->then(function () use ($package): void {
+                $this->filesystem->remove($this->getInstallPath($package));
+                $this->io->write(sprintf('  - %s, removing symlink', UninstallOperation::format($package)));
+            });
+        }
 
-	/**
-	 * Returns the global installer base path.
-	 * @since 1.0.0
-	 * @return string The path.
-	 */
-	public function getBasePath(): string {
-		return $this->globalPath;
-	}
+        return $promise;
+    }
 
-	/**
-	 * Returns the global path for a package.
-	 * @since 1.0.0
-	 * @param PackageInterface $package Package.
-	 * @return string The path.
-	 */
-	private function getGlobalPath(PackageInterface $package): ?string {
-		return sprintf('%s/%s/%s', $this->getBasePath(), $package->getPrettyName(), $package->getPrettyVersion());
-	}
+    /**
+     * Returns the global installer base path.
+     * @since 1.0.0
+     * @return string The path.
+     */
+    public function getBasePath(): string
+    {
+        return $this->globalPath;
+    }
 
-	/**
-	 * Checks if a package supports a global path.
-	 * @since 1.0.0
-	 * @param PackageInterface $package The package.
-	 * @return bool If a global path is supported.
-	 */
-	private function supportsGlobalPath(PackageInterface $package): bool {
-		if ('stable' !== $package->getStability()) {
-			return false;
-		}
+    /**
+     * Returns the global path for a package.
+     * @since 1.0.0
+     * @param PackageInterface $package Package.
+     * @return string The path.
+     */
+    private function getGlobalPath(PackageInterface $package): ?string
+    {
+        return sprintf('%s/%s/%s', $this->getBasePath(), $package->getPrettyName(), $package->getPrettyVersion());
+    }
 
-		return !in_array($package->getPrettyName(), $this->excludedPackages, true);
-	}
+    /**
+     * Checks if a package supports a global path.
+     * @since 1.0.0
+     * @param PackageInterface $package The package.
+     * @return bool If a global path is supported.
+     */
+    private function supportsGlobalPath(PackageInterface $package): bool
+    {
+        if ('stable' !== $package->getStability()) {
+            return false;
+        }
+
+        return !in_array($package->getPrettyName(), $this->excludedPackages, true);
+    }
 }
