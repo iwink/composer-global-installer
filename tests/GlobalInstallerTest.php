716ca16b06f46cee1110468d46f3ffb407c32e99
Doeke Norg <d.norg@iwink.nl>
2021-12-03 19:46:06 +0100

    Fix unit tests

--- tests/GlobalInstallerTest.php
+++ tests/GlobalInstallerTest.php
@@ -12,79 +12,90 @@ use PHPUnit\Framework\TestCase;
 /**
  * Unit tests for {@see GlobalInstaller}.
  */
-class GlobalInstallerTest extends TestCase {
-	/**
-	 * The composer instance.
-	 * @since $ver$
-	 * @var Composer
-	 */
-	private Composer $composer;
+class GlobalInstallerTest extends TestCase
+{
+    /**
+     * The composer instance.
+     * @since $ver$
+     * @var Composer
+     */
+    private Composer $composer;
 
-	/**
-	 * The installer under test.
-	 * @since $ver$
-	 * @var GlobalInstaller
-	 */
-	private GlobalInstaller $installer;
+    /**
+     * The installer under test.
+     * @since $ver$
+     * @var GlobalInstaller
+     */
+    private GlobalInstaller $installer;
 
-	/**
-	 * @inheritDoc
-	 * @since $ver$
-	 */
-	protected function setUp(): void {
-		parent::setUp();
+    /**
+     * @inheritDoc
+     * @since $ver$
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
 
-		$io = new NullIO();
+        $io = new NullIO();
 
-		$this->composer = new Composer();
-		$this->composer->setConfig(Factory::createConfig($io));
-		$this->installer = new GlobalInstaller(
-			$io,
-			$this->composer,
-			'/project-dir',
-			'/global-dir',
-			(object) [
-				'stabilities' => ['stable'],
-			],
-		);
-	}
+        $this->composer = new Composer();
+        $config = Factory::createConfig($io);
+        $config->merge([
+            'config' => [
+                'vendor-dir' => '/tmp/composer',
+            ]
+        ]);
+        $this->composer->setConfig($config);
+        $this->installer = new GlobalInstaller(
+            $io,
+            $this->composer,
+            '/project-dir',
+            '/global-dir',
+            (object)[
+                'stabilities' => ['stable'],
+            ],
+        );
+    }
 
-	/**
-	 * Test case for {@see GlobalInstaller::supports()}.
-	 * @since $ver$
-	 * @testWith ["composer-installer"]
-	 *           ["composer-plugin"]
-	 *           ["metapackage"]
-	 */
-	public function testSupports(string $package): void {
-		self::assertFalse($this->installer->supports($package));
-	}
+    /**
+     * Test case for {@see GlobalInstaller::supports()}.
+     * @since $ver$
+     * @testWith ["composer-installer"]
+     *           ["composer-plugin"]
+     *           ["metapackage"]
+     */
+    public function testSupports(string $package): void
+    {
+        self::assertFalse($this->installer->supports($package));
+    }
 
-	/**
-	 * Unit test for {@see GlobalInstaller::getInstallPath()} with allowed path.
-	 * @since $ver$
-	 */
-	public function testGetInstallPathWithAllowed(): void {
-		$package = $this->createConfiguredMock(PackageInterface::class, [
-			'getPrettyName' => 'my-package',
-			'getPrettyVersion' => '1.0.1',
-			'getStability' => 'stable',
-		]);
+    /**
+     * Unit test for {@see GlobalInstaller::getInstallPath()} with allowed path.
+     * @since $ver$
+     */
+    public function testGetInstallPathWithAllowed(): void
+    {
+        $package = $this->createConfiguredMock(PackageInterface::class, [
+            'getPrettyName' => 'my-package',
+            'getPrettyVersion' => '1.0.1',
+            'getStability' => 'stable',
+        ]);
 
-		self::assertSame('/global-dir/my-package/1.0.1', $this->installer->getInstallPath($package));
-	}
+        self::assertSame('/global-dir/my-package/1.0.1', $this->installer->getInstallPath($package));
+    }
 
-	/**
-	 * Unit test for {@see GlobalInstaller::getInstallPath()} with non-allowed path.
-	 * @since $ver$
-	 */
-	public function testGetInstallPathWithoutAllowed(): void {
-		$package = $this->createConfiguredMock(PackageInterface::class, [
-			'getPrettyName' => 'my-package',
-			'getPrettyVersion' => '1.0.1',
-			'getStability' => 'dev',
-		]);
+    /**
+     * Unit test for {@see GlobalInstaller::getInstallPath()} with non-allowed path.
+     * @since $ver$
+     */
+    public function testGetInstallPathWithoutAllowed(): void
+    {
+        $package = $this->createConfiguredMock(PackageInterface::class, [
+            'getPrettyName' => 'my-package',
+            'getPrettyVersion' => '1.0.1',
+            'getStability' => 'dev',
+        ]);
 
-		self::assertSame(__DIR__ . '/vendor/my-package', $this->installer->getInstallPath($package));
-	}
+        self::assertSame(realpath('/tmp/composer').'/my-package', $this->installer->getInstallPath($package));
+    }
 }
